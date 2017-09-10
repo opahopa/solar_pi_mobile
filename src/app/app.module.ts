7d4cd10ad17d87e629913d977af148ef68181721
opahopa <opahopa@gmail.com>
2017-09-10 11:04:40 +0700

    3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ChartsModule } from 'ng2-charts';
@@ -20,10 +15,6 @@ import {HttpModule} from "@angular/http";
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -35,10 +26,6 @@ import {HttpModule} from "@angular/http";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
   ],
   providers: [
     StatusBar,
